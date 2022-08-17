380ed42d745251d272baea41998e5e4ccb5422ff
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-08-17 04:14:35 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -17,23 +17,30 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Quiz App'),),
+      appBar: AppBar(
+        title: const Text('Quiz App'),
+      ),
       body: SafeArea(
         child: Container(
           padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              ElevatedButton(onPressed: (){}, child: const Text('Answer 1'),),
-              const SizedBox(height: 20,),
-              ElevatedButton(onPressed: (){}, child: const Text('Answer 1'))
-
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Answer 1'),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text('Answer 1'))
             ],
           ),
         ),
@@ -41,4 +48,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-
