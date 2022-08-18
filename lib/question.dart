f218184bfbebeb6ca9f703f85c7c114fa2411dd1
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-08-18 04:38:54 +0100

    Update question.dart

--- lib/question.dart
+++ lib/question.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
 class Questions extends StatelessWidget {
- final String? questionText;
-  const Questions(String question, {Key? key, this.questionText}) : super(key: key);
+  final String? questionText;
+
+  const Questions(String question, {Key? key, this.questionText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText!, style: const TextStyle(),);
+    return Text(
+      questionText!,
+      style: const TextStyle(fontSize: 20),
+      textAlign: TextAlign.center,
+    );
   }
 }
