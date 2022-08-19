86e1747eae29ea465dc1a746fbbb340ffeb2fa41
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-08-19 02:58:48 +0100

    Updated

---
+++ lib/answer.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Answers extends StatelessWidget {
+  const Answers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed:  (){},
+          child: Text(''),
+      ),
+    );
+  }
+}
--- lib/question.dart
+++ lib/question.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 
 class Questions extends StatelessWidget {
-  final String? questionText;
+ String questionText;
 
-  const Questions(String question, {Key? key, this.questionText})
+  Questions(String question, {Key? key, this.questionText= ''})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Text(
-      questionText!,
+      questionText,
       style: const TextStyle(fontSize: 20),
       textAlign: TextAlign.center,
     );
